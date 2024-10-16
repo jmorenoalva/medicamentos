e421403bc4bafee06b7bd945ebc03bdc369f97cb
Javier Moreno <jamorenoalva@gmail.com>
2024-10-16 01:26:37 -0500

    Se adiciono el manejo de messageConfig. Se adiciono PaginaController y todos sus metodos Http

--- src/main/java/com/morealva/config/MapperConfig.java
+++ src/main/java/com/morealva/config/MapperConfig.java
@@ -1,4 +1,14 @@
 package com.morealva.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MapperConfig {
+
+    @Bean(name = "defaultMapper")
+    public ModelMapper defaultMapper(){
+        return new ModelMapper();
+    }
 }
--- src/main/java/com/morealva/controller/PaginaController.java
+++ src/main/java/com/morealva/controller/PaginaController.java
@@ -4,13 +4,13 @@ import com.morealva.dto.PaginaDTO;
 import com.morealva.modelo.Pagina;
 import com.morealva.service.IPaginaService;
 import com.morealva.util.MapperUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,4 +33,27 @@ public class PaginaController {
 
         return ResponseEntity.ok(mapperUtil.map(obj, PaginaDTO.class));
     }
+
+    @PostMapping
+    public ResponseEntity<Void> save(@Valid @RequestBody PaginaDTO paginaDTO) {
+        Pagina obj = paginaService.save(mapperUtil.map(paginaDTO, Pagina.class));
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getIdPagina()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PaginaDTO> update(@Valid @PathVariable("id") Integer id, @RequestBody PaginaDTO paginaDTO) {
+        paginaDTO.setIdPagina(id);
+        Pagina obj=paginaService.update(id, mapperUtil.map(paginaDTO, Pagina.class));
+
+        return ResponseEntity.ok(mapperUtil.map(obj, PaginaDTO.class));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
+        paginaService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+    
 }
